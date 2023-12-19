678df149953be907beaa2a5375b6a0e82fbad90b
DESKTOP-M9FDRN9\csi23-iloposa <csi23-iloposa@colegioaltair.net>
2023-12-19 11:17:30 +0100

    no message

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ejercicio4ArraysVectores;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int numeroUsu;
+        Console.WriteLine("Escriba eltamaño del vector ");
+        numeroUsu = Convert.ToInt32(Console.ReadLine());
+    
+        double[] matriz = new double[numeroUsu];
+       
+
+        Random numero = new Random();
+
+        for (int i = 0; i < matriz.Length; i++)
+        {
+            matriz[i] = numero.Next();
+
+            Console.WriteLine(matriz[i]);
+        }
+        
+    }
+}
